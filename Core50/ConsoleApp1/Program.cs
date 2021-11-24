bb23f80aa455318300db9e79b64d488ebf0a4a64
viktor <viktortat@gmail.com>
2021-11-24 20:04:59 +0200

    + Простейший

--- Core50/ConsoleApp1/Program.cs
+++ Core50/ConsoleApp1/Program.cs
@@ -2,11 +2,42 @@
 
 namespace ConsoleApp1
 {
+    public class ClassA
+    {
+        //public void DoWork()
+        //{
+        //    var b = new ClassB();
+        //    b.DoStuff();
+        //}
+
+        private readonly IThing _dependency;
+
+        public ClassA(IThing thing) => _dependency = thing;
+        public void DoWork() => _dependency.DoStuff();
+    }
+    
+    public class ClassB : IThing
+    {
+        public void DoStuff()
+        {
+            
+            // Imagine implementation
+            Console.WriteLine($"ClassB:DoStuff");
+        }
+    }
+
+    public interface IThing
+    { 
+        void DoStuff();
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IThing thing = new ClassB();
+            ClassA classA = new ClassA(thing);
+            classA.DoWork();
         }
     }
 }
